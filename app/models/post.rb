49251658714b0382fa829b2b5626391f79176807
yash-dani <daniyash19@gmail.com>
2020-04-29 19:40:44 -0400

    revised db architecture to speedup dev time

--- app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ApplicationRecord
-    validates :title, uniqueness: true
-    validates :link, uniqueness:true
-    has_many: comment
-	belongs_to: user
-end
--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-	validates :username, presence: true, uniqueness: true
-	has_many :comments
-	has_many :posts
-end
